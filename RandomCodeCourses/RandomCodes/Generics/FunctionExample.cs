eccd4c2e584aff7117d7a2ba034648796d3d3555
Diogo Feld <cfeld.diogo@gmail.com>
2025-05-10 15:46:10 -0300

    random code

--- RandomCodeCourses/RandomCodes/Generics/FunctionExample.cs
+++ RandomCodeCourses/RandomCodes/Generics/FunctionExample.cs
@@ -6,88 +6,45 @@ using System.Reflection;
 using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace RandomCodes.Generics
 {
     static class FunctionExample
     {
-        static void FunctionExampleRun()
-        {
-            List<Employee> listE = new List<Employee>
-            {
-            new Employee(),
-            new Employee(),
-            new Employee()
-            };
 
-            SortedList<Employee> sortedList = new SortedList<Employee>(listE);
-        }
     }
 
-    public class SortedList<T> where T : Employee, IComparable<T>//restring para o employee e IComparable
+
+    public class Exercise
     {
-        public IEnumerable<T> Items { get; }
-
-        public SortedList(IEnumerable<T> items)
+        public void TestMethod()
         {
-            var asList = items.ToList();
-            asList.Sort();
-            Items = asList;
+            Func<int, bool, double> someMethod1 = Method1;
+            Func<DateTime> someMethod2 = Method2;
+            Action<string, string> someMethod3 = Method3;
         }
+
+        public double Method1(int a, bool b) => 0;
+        public DateTime Method2() => default(DateTime);
+        public void Method3(string a, string b) { }
     }
 
-    public class Employee : IComparable<Employee>
+
+    public class Exercise2
     {
-        public int CompareTo(Employee? other)
-        {
-            throw new NotImplementedException();
-        }
+        public Func<string, int> GetLength = s => s.Length;
+        public Func<int> GetRandomNumberBetween1And10 = () => new Random().Next(1, 11);// your code goes here;
+                                                                                       
     }
 
-    public class FullName : IComparable<FullName> // your code goes here
-    {
-        public string FirstName { get; init; }
-        public string LastName { get; init; }
-
-        public int CompareTo(FullName? other)
-        {
-
-            var result = LastName.CompareTo(other.LastName);
-            if (result == 0)
-            {
-                return FirstName.CompareTo(other.FirstName);
-            }
-            return result;
-        }
-
-        public override string ToString() => $"{FirstName} {LastName}";
-
-        //your code hoes here
-    }
 
 
     /*
-Type constraints & IComparable - SortedList of FullNames
-Finalize the implementation of the given program.
+     GetLength stores a function taking a string and returning its length.
+    
+    GetRandomNumberBetween1And10 stores a parameterless function generating a random number between 1 and 10.
+     
+     */
 
-
-
-The SortedList is a wrapper for a collection. It sorts the collection given in the constructor using the Sort method.
-
-The FullName class holds two string properties - FirstName and LastName.
-
-
-
-We want to be able to create a SortedList<FullName> object. The names stored in the Items collection should be sorted first by LastName, then by FirstName.
-For example, for the following collection of FullNames passed as the constructor argument:
-John Smith, Anna Smith, Kenji Narasaki, John Watson
-
-The Items collection of the SortedList should contain FullNames objects in the following order:
-Kenji Narasaki, Anna Smith, John Smith, John Watson
-Kenji Narasaki is first because his last name is first in alphabetical order. Two people with the same last name (John Smith and Anna Smith) are ordered by their first names. The last in the order is John Watson because his last name is last in alphabetical order.
-
-You will need to:
-Specify a proper type constraint in the SortedList class. Only classes that can be ordered should be used as the T parameter.
-Make FullName class implement a proper interface that will allow the ordering of objects of this class.
-    */
 }
--- RandomCodeCourses/RandomCodes/Generics/GenericsEx.cs
+++ RandomCodeCourses/RandomCodes/Generics/GenericsEx.cs
@@ -22,5 +22,61 @@ namespace RandomCodes.Generics
 
         }
 
+        static void FunctionExampleRun()
+        {
+            List<Employee> listE = new List<Employee>
+            {
+            new Employee(),
+            new Employee(),
+            new Employee()
+            };
+
+            SortedList<Employee> sortedList = new SortedList<Employee>(listE);
+        }
+
+    }
+
+
+
+
+    public class SortedList<T> where T : Employee, IComparable<T>//restring para o employee e IComparable
+    {
+        public IEnumerable<T> Items { get; }
+
+        public SortedList(IEnumerable<T> items)
+        {
+            var asList = items.ToList();
+            asList.Sort();
+            Items = asList;
+        }
+    }
+
+    public class Employee : IComparable<Employee>
+    {
+        public int CompareTo(Employee? other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class FullName : IComparable<FullName> // your code goes here
+    {
+        public string FirstName { get; init; }
+        public string LastName { get; init; }
+
+        public int CompareTo(FullName? other)
+        {
+
+            var result = LastName.CompareTo(other.LastName);
+            if (result == 0)
+            {
+                return FirstName.CompareTo(other.FirstName);
+            }
+            return result;
+        }
+
+        public override string ToString() => $"{FirstName} {LastName}";
+
+        //your code hoes here
     }
 }
