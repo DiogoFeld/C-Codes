84b39091fab36913492ec3dd74a7c18f93bc2ff7
Diogo Feld <cfeld.diogo@gmail.com>
2025-09-04 19:19:40 -0300

    a

---
+++ RandomCodeCourses/RandomCodes/Evolution/cSharp11RequiredEx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomCodes.Evolution
+{
+    internal class cSharp11RequiredEx
+    {
+
+        //Car c = new Car();
+        //reclama da falta de Plcaca
+        //Car c = new Car{ Placa = "done"};
+        //ou com setsRequiredMenbers
+        Car c = new Car("donee");
+
+
+    }
+
+
+    class Car
+    {
+        [SetsRequiredMembers]
+        public Car(string placa)
+        {
+            Placa = placa;
+        }
+
+
+
+        //public string Placa { get; set; }
+        //mas é NECESSARIO ter placa
+        public required string Placa { get; init; }
+    }
+}
---
+++ RandomCodeCourses/RandomCodes/Evolution/cSharp11Strings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomCodes.Evolution
+{
+    internal class cSharp11Strings
+    {
+        string rawString = @"pode ser 
+        dividida
+        sem precisar
+        de indentação";
+
+
+        string rawString2 = """ podem ser separadoas em triple quotes ou mais tambpem """;
+
+
+
+
+    }
+}
