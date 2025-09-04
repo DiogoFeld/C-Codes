0063cad0411486d40bcfb516538c886aa2fd9cab
Diogo Feld <cfeld.diogo@gmail.com>
2025-09-04 19:45:17 -0300

    c

---
+++ RandomCodeCourses/RandomCodes/Evolution/cSharp12Collection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomCodes.Evolution
+{
+    internal class cSharp12Collection
+    {
+        public void showMethods()
+        {
+            int[] number = [1, 2, 3, 4];
+            List<int> list = [1, 2, 3, 4];
+            HashSet<int> set = [.. list, ..number];
+        }
+       
+    }
+
+
+
+
+}
---
+++ RandomCodeCourses/RandomCodes/Evolution/cSharp12Constructor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomCodes.Evolution
+{
+    internal class cSharp12Constructor
+    {
+        Bird bird = new Bird("bird");
+        //bird.nome;            // simplify constructor
+        Goose goose = new Goose("gooose");
+        Person p = new Person("f", "l");
+        public void w()
+        {
+            bird.SayName();
+            //bird.nome;
+            Console.WriteLine(goose.nome);
+            Console.WriteLine(p.FirstName);
+            Console.WriteLine(p);
+        }
+
+    }
+
+
+
+    class Bird(string nome)
+    {
+        public void SayName()
+        {
+            Console.Write("my name is " +nome);
+        }
+    };
+
+
+    public record Goose(string nome);
+    public record Person(string FirstName, string LastName);
+
+
+}
