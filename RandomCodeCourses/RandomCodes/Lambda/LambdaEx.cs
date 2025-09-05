fec5a591ce5006a3c1f33ccbbc79f50ee5e7efb8
Diogo Feld <cfeld.diogo@gmail.com>
2025-09-05 00:03:05 -0300

    b

---
+++ RandomCodeCourses/RandomCodes/Lambda/LambdaEx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomCodes.Lambda
+{
+    internal class LambdaEx
+    {
+        public void ExampleLambda()
+        {
+            var operacoes = new List<Func<int,int, int>>
+        {
+            (x,y) => x + 1,     // soma 1
+            (x,y) => x * 2,     // multiplica por 2
+            (x,y) => x * x      // quadrado
+        };
+
+            int numero = 5;
+            int numero2 = 1;
+
+            foreach (var op in operacoes)
+            {
+                Console.WriteLine(op(numero, numero2));
+            }
+        }
+    }
+}
