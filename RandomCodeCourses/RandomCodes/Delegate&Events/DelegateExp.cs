e297d427b58089d8db21e6194e189553d0afe8e9
Diogo Feld <cfeld.diogo@gmail.com>
2025-09-16 23:20:06 -0300

    c

---
+++ RandomCodeCourses/RandomCodes/Delegate&Events/DelegateExp.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomCodes.Delegate_Events
+{
+    public static class DelegateExp
+    {
+
+    }
+
+    public delegate void OnBalanceDecreased(decimal balance);
+
+    public class BankAccount
+    {
+        public decimal Balance { get; private set; }
+        public BankAccount(decimal initialBalance)
+        {
+            Balance = initialBalance;
+        }
+
+        public event OnBalanceDecreased OnBalanceDecreased;
+        public void DecreaseBalance(decimal price)
+        {
+            Balance -= price;
+            OnBalanceDecreased?.Invoke(price);
+            //your code goes here
+        }
+    }
+
+    public class User
+    {
+        public decimal Funds { get; private set; }
+
+        public User(decimal cash, decimal moneyInBank)
+        {
+            Funds = cash + moneyInBank;
+        }
+        public void ReduceFunds(decimal balanceReduced)
+        {
+            Funds -= balanceReduced;
+            //your code goes here
+        }
+    }
+
+
+
+}
